df1d110b3e9ce36fbaf119cb53e09ed07e34827d
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-17 08:45:06 -0700

    Basic design finished

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,13 @@
+import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
+import PopularMenu from "../PopularMenu/PopularMenu";
 
 const Home = () => {
     return (
         <div>
-          <h2>This is My Home</h2>  
+          <Banner></Banner> 
+          <Category></Category>
+          <PopularMenu></PopularMenu>
         </div>
     );
 };
