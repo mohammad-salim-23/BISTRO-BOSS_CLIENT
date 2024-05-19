7eb683b2f96ebd4a130326d90e10c24d8f595553
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-18 23:31:20 -0700

    for review data localhost add

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,22 +1,30 @@
-
-
-const FoodCard = ({item}) => {
-    const {name,image,price,recipe} = item;
-    console.log(item);
-    return (
-        <div>
-           <div className="card card-compact w-96 bg-base-100 shadow-xl">
-  <figure><img src={image} alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div> 
+const FoodCard = ({ item }) => {
+  const { name, image, price, recipe } = item;
+  console.log(item);
+  return (
+    <div>
+      <div className="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img src={image} alt="Shoes" />
+        </figure>
+        <p
+          className="bg-slate-900 text-white absolute right-0
+  mr-4 mt-4 px-4"
+        >
+          ${price}
+        </p>
+        <div className="card-body flex flex-col items-center">
+          <h2 className="card-title">{name}</h2>
+          <p>{recipe}</p>
+          <div className="card-actions justify-end">
+            <button className="btn bg-slate-100 border-0 border-b-4 border-orange-400 text-orange-400">
+              Add to Cart
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default FoodCard;
--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -6,26 +6,26 @@ import { useState } from "react";
 import useMenu from "../../../hooks/useMenu";
 import { useParams } from "react-router-dom";
 import { Helmet } from "react-helmet";
-import FoodCard from "../../../Components/FoodCard/FoodCard";
+
+import OrderTab from "./OrderTab";
 
 const Order = () => {
-    const categories = ['salad','pizza','soup','dessert','drinks'];
-    const {category} = useParams();
-    const initialIndex = categories.indexOf(category);
-    const [tabIndex,setTabIndex] = useState(initialIndex);
-    const [menu] = useMenu();
-    
-    
-    const desert = menu.filter((item) => item.category === "dessert");
-    const salad = menu.filter((item) => item.category === "salad");
-    const soup = menu.filter((item) => item.category === "soup");
-    const pizza = menu.filter((item) => item.category === "pizza");
-    const offered = menu.filter((item) => item.category === "offered");
+  const categories = ["salad", "pizza", "soup", "dessert", "drinks"];
+  const { category } = useParams();
+  const initialIndex = categories.indexOf(category);
+  const [tabIndex, setTabIndex] = useState(initialIndex);
+  const [menu] = useMenu();
+
+  const desert = menu.filter((item) => item.category === "dessert");
+  const salad = menu.filter((item) => item.category === "salad");
+  const soup = menu.filter((item) => item.category === "soup");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const drinks = menu.filter((item) => item.category === "drinks");
   return (
     <div>
-         <Helmet>
-                <title>Bistro Boss | Order Food</title>
-            </Helmet>
+      <Helmet>
+        <title>Bistro Boss | Order Food</title>
+      </Helmet>
       <Cover img={orderCover} title={"Order food"}></Cover>
       <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
         <TabList>
@@ -36,21 +36,34 @@ const Order = () => {
           <Tab>Drinks</Tab>
         </TabList>
         <TabPanel>
-        {salad.map((item, index) => (
-                        <FoodCard key={index} item={item} />
-                    ))}
+           <OrderTab
+           items={salad}
+           >
+           </OrderTab>
         </TabPanel>
         <TabPanel>
-        <FoodCard item={pizza}></FoodCard>
+        <OrderTab
+           items={pizza}
+           >
+           </OrderTab>
         </TabPanel>
         <TabPanel>
-        <FoodCard item={soup}></FoodCard>
+        <OrderTab
+           items={soup}
+           >
+           </OrderTab>
         </TabPanel>
         <TabPanel>
-        <FoodCard item={desert}></FoodCard>
+        <OrderTab
+           items={desert}
+           >
+           </OrderTab>
         </TabPanel>
         <TabPanel>
-        {/* <FoodCard item={drinks}></FoodCard> */}
+        <OrderTab
+           items={drinks}
+           >
+           </OrderTab>
         </TabPanel>
       </Tabs>
     </div>
