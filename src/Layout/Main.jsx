1d04cfc97fb658a8602ba1ca1942759783098c86
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-19 04:10:44 -0700

    Login page add

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,14 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../Shared/Footer/Footer";
 import Navbar from "../Shared/Footer/Navbar/Navbar";
 
 
 const Main = () => {
+    const location = useLocation();
+    const noHeaderFooter = location.pathname.includes('login');
     return (
         <div>
-            <Navbar></Navbar>
+          {noHeaderFooter ||   <Navbar></Navbar>}
            <Outlet></Outlet> 
-           <Footer></Footer>
+         {  noHeaderFooter ||<Footer></Footer>}
         </div>
     );
 };
