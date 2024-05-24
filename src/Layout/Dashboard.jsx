73ee1311e3e761833ad06b6671c8aa7ce15cbd55
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-24 03:08:44 -0700

    AllUsers component create

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,22 +1,67 @@
 import {
   FaAd,
+  FaBook,
   FaCalendar,
+  FaEnvelope,
   FaHome,
   FaList,
   FaSearch,
  
   FaShoppingCart,
+  FaUbuntu,
+  FaUsers,
+  FaUtensils,
 } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
   const [cart] = useCart();
+  // TODO: get isAdmin value from the database
+  const isAdmin = true;
   return (
     <div className="flex">
       {/*dashboard side bar */}
       <div className="w-64 min-h-screen bg-[#D1A054]">
         <ul className="menu">
+         {
+          isAdmin ?
+          <>
+             <li>
+            <NavLink to="/dashboard/adminHome">
+              <FaHome></FaHome>
+              Admin Home
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/dashboard/addItems">
+              <FaUtensils></FaUtensils>
+              Add Items
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/dashboard/manageItems">
+              <FaList></FaList>
+             Manage Items
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/bookings">
+            <FaBook></FaBook>
+              Manage Bookings
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/users">
+              <FaUsers></FaUsers>
+              All Users
+            </NavLink>
+          </li>
+
+        
+          </> : <>
           <li>
             <NavLink to="/dashboard/userHome">
               <FaHome></FaHome>
@@ -51,7 +96,10 @@ const Dashboard = () => {
               My Bookings
             </NavLink>
           </li>
+          </>
+         }
           <div className="divider"></div>
+          {/* shared nav links */}
           <li>
             <NavLink to="/">
               <FaHome></FaHome>
@@ -62,6 +110,12 @@ const Dashboard = () => {
             <NavLink to="/order/salad">
               <FaSearch></FaSearch>
              Menu
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">
+              <FaEnvelope></FaEnvelope>
+             Contact
             </NavLink>
           </li>
          
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -12,6 +12,7 @@ import PrivateRoute from "./PrivateRoute/PrivateRoute";
 import Secret from "../Secret/Secret";
 import Dashboard from "../Layout/Dashboard";
 import Cart from "../pages/Dashboard/Cart/Cart";
+import AllUsers from "../pages/Dashboard/AllUsers/AllUsers";
 export const router = createBrowserRouter([
     {
       path: "/",
@@ -53,6 +54,10 @@ export const router = createBrowserRouter([
           path:'cart',
           element:<Cart></Cart>,
           
+        },
+        {
+          path:'users',
+          element:<AllUsers></AllUsers>
         }
       ]
     }
