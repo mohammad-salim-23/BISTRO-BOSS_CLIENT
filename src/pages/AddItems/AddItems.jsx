b7e35e0032d77c9dd31ae86517eeb32bb8008030
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-25 22:53:38 -0700

    Save menu item to the server

--- src/pages/AddItems/AddItems.jsx
+++ src/pages/AddItems/AddItems.jsx
@@ -1,10 +1,42 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../Components/SectionTitle/SectionTitle";
-
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+ const image_hosting_key = import.meta.env.  VITE_IMAGE_HOSTING_KEY;
+ const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key }`
 const AddItems = () => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
+  const axiosPublic = useAxiosPublic();
+  const axiosSecure  = useAxiosSecure();
+  const onSubmit = async(data) => {
     console.log(data);
+    // image upload to imgbb and then get an url
+    const imageFile = {image:data.image[0]}
+  const res = await axiosPublic.post(image_hosting_api,imageFile,{
+    headers:{
+      'content-type':'multipart/form-data',
+    }
+  });
+  if(res.data.success){
+    // now send the menu item data to the server with the imageURL
+    const menuItem = {
+      name:data.name,
+      category :data.category,
+      price:parseFloat(data.price),
+      recipe:data.recipe,
+      image:res.data.data.display_url,
+    }
+   
+    const menuRes = await axiosSecure.post('/menu',menuItem);
+    console.log(menuRes.data);
+    if(menuRes.data.insertedId){
+      // show success popup
+    }
+
+  }
+
+  console.log("with image_url",res.data);
+
   };
   return (
     <div>
@@ -32,11 +64,11 @@ const AddItems = () => {
             <div className="label">
               <span className="label-text">Category*</span>
             </div>
-            <select
+            <select defaultValue="default"
             {...register("category",{ required: true})}
             className="select select-bordered w-full "
           >
-          <option disabled selected>
+          <option disabled value="default">
             Select a category
           </option>
           <option value="salad">Salad</option>
